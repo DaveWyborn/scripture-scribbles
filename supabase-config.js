1c7c4369c2219d89c747f278a43f92bc7cbb6c22
DaveWyborn <dwyborn@gmail.com>
2026-03-17 18:58:33 +0000

    Fix anon key typo - extra s in signature

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,5 @@
 
 const SUPABASE_CONFIG = {
     url: 'https://qzenmfbqjmraknmakgel.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6ZW5tZmJxam1yYWtubWFrZ2VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTE0MDEsImV4cCI6MjA3ODk2NzQwMX0.76cmKHheDBoUk1rMwOV0EypQ1TVdJDN8nsPsTrERwMdg'
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6ZW5tZmJxam1yYWtubWFrZ2VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTE0MDEsImV4cCI6MjA3ODk2NzQwMX0.76cmKHheDBoUk1rMwOV0EypQ1TVdJDN8nPsTrERwMdg'
 };
